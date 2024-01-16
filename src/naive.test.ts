b253abf679e5ed94522af73c4e7763cfb3cd8b54
Max Gurewitz <max@dittofeed.com>
2024-01-15 21:04:16 -0800

    wip

--- src/naive.test.ts
+++ src/naive.test.ts
@@ -1,9 +1,14 @@
 import fs from "fs/promises";
 import path from "path";
+import { createClient } from "@clickhouse/client";
 
 describe("example", () => {
   beforeAll(async () => {
     const sql = await fs.readFile(path.join(__dirname, "naive.sql"), "utf-8");
+    const ch = createClient({
+      host: "http://localhost:8123",
+      database: "segmentation",
+    });
     console.log("loc1", sql);
   });
 
