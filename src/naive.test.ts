541ad05fc46290a3cbfcf9c12fb4788257f57cb7
Max Gurewitz <max@dittofeed.com>
2024-01-15 20:41:33 -0800

    wip

--- src/naive.test.ts
+++ src/naive.test.ts
@@ -1,4 +1,12 @@
+import fs from "fs/promises";
+import path from "path";
+
 describe("example", () => {
+  beforeAll(async () => {
+    const sql = await fs.readFile(path.join(__dirname, "naive.sql"), "utf-8");
+    console.log("loc1", sql);
+  });
+
   it("should be true", () => {
     expect(true).toBe(true);
   });
