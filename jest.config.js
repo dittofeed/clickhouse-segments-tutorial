c56eda8a57cab6110dad0fd67349ac1e398f09ff
Max Gurewitz <max@dittofeed.com>
2024-01-15 22:57:36 -0800

    wip

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,7 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  testEnvironment: "node",
+  globalTeardown: "<rootDir>/src/jestGlobalTeardown.ts",
+  globalSetup: "<rootDir>/src/jestGlobalSetup.ts",
 };
